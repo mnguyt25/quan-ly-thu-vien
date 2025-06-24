a2e357cb4ab2503e2c1f94460731ff8203dafd17
mnguyt25 <nguyethtm3590@ut.edu.vn>
2025-06-25 00:01:51 +0700

    Delete project directory

--- project/main.py
+++
@@ -1,5 +0,0 @@
-from customtkinter import set_appearance_mode # import hàm đổi giao diện
-from ui.login import open_login_window
-
-set_appearance_mode("light") # Kích hoạt chế độ giao diện sáng cho toàn bộ giao diện dùng 'customtkinter'
-open_login_window() # Mở giao diện đăng nhập
